c281c9926a7b2f0638ab7391667b3e9ce2afb978
V-Yashasvi <vyashasvi1@gmail.com>
2025-01-29 11:10:20 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const { resolve } = require('path');
-
+const mongoose= require('mongoose')
+const dotenv= require('dotenv')
+dotenv.config()
 const app = express();
+
 const port = 3010;
+const mongoURL=process.env.url
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  res.send("Home page")
 });
 
-app.listen(port, () => {
+app.listen(port, async() => {
+  try{
+    await mongoose.connect(mongoURL)
+    console.log("Connected to database")
+  }catch(error){
+    console.log("Error connecting to database:",error)
+  }
   console.log(`Example app listening at http://localhost:${port}`);
 });
